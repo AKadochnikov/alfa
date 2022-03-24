22a3903477ec5a3b452f061135cb4762eae2be25
Alexander <alex.kadoh@mail.ru>
2022-03-24 17:10:35 +0700

    delete markup directory, alfa-bank logo and refactor style.css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,11 @@
 import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 function Header ():JSX.Element {
   return (
     <header className="header-content padding-container">
-      <Link aria-current="page" to={'#'}>
-        <img src="/images/alfa-logo.svg" width="227" height="80" alt="Логотип Альфа-банка."/>
+      <Link aria-current="page" to={AppRoute.Main}>
+        <img src="/images/alfa-digital.svg" width="279" height="62" alt="Логотип Альфа-Digital."/>
       </Link>
     </header>
   );
